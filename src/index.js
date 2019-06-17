e141148b9615fe64f059ac2c057e43adfc07fc6d
姜通 <jiangtong@sunmnet.com>
2019-06-17 11:06:22 +0800

    hooks eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-/**
- * Created by yangqian on 2017/11/20.
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router} from 'react-router-dom';
-import createHistory from 'history/createHashHistory';
 import store from '@useRedux/store';
 import {Provider} from 'react-redux';
 import Root from './router/router';
 import {LocaleProvider} from 'antd';
+import {HashRouter as Router} from 'react-router-dom';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import echartsConfig from '@utils/echartsConfig';
 import echarts from 'echarts';
@@ -18,14 +13,12 @@ import chinaMap from 'echarts/map/json/china.json';
 echarts.registerTheme('echartsConfig', echartsConfig);
 echarts.registerMap('china', chinaMap);
 
-const history = createHistory();
-
 class App extends React.Component {
     render() {
         return (
             <LocaleProvider locale={zh_CN}>
                 <Provider store={store}>
-                    <Router history={history}>
+                    <Router>
                         <Root/>
                     </Router>
                 </Provider>
